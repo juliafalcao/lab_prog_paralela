06d83b3ea26314373063f5bcd614b18df060bea6
Júlia Falcão <juliafsfalcao@hotmail.com>
2019-07-02 10:14:17 -0300

    mpi funcionando agora

--- trab_final/mpi_puro.c
+++ trab_final/mpi_puro.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <mpi.h>
 
-int main(int *argc, int argv[]) {
+int main(int argc, char **argv) {
 
 	double soma, soma_local, valor;
 	int n = 256; // tamanho do vetor
@@ -13,36 +13,36 @@ int main(int *argc, int argv[]) {
 
 	double a[n], b[n];
 
-	MPI_Init(&argc, &argv);    
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
 
 	int n_local = n / np;
 	int resto = n % np;
 	
 	
-
 	for (i = my_rank*n_local; i < (my_rank+1)*n_local; i++) { // inicialização do vetor
 		a[i] = i * 0.5;
 		b[i] = i * 2.0;
 	}
 
 
-	for (i = my_rank*n_local; i < (my_rank+1)*n_local; i++) {
+	for (i = my_rank*n_local; i < (my_rank+1)*n_local; i++) { // cálculo da soma local
 		valor = a[i]*b[i];
 		soma_local += valor;
 	}
 
-	if (my_rank < resto) {
+	if (my_rank < resto) { // cálculo do resto
 		int offset = n_local*np;
+		int i = offset+my_rank;
 
-		a[offset+my_rank] = i * 0.5;
-		b[offset+my_rank] = i * 2.0;
-		valor = a[offset+my_rank] * b[offset+my_rank];
+		a[i] = i * 0.5;
+		b[i] = i * 2.0;
+		valor = a[i] * b[i];
 		soma_local += valor;
 	}
 
-	if (my_rank == 0) {
+	if (my_rank == 0) { // master recebe as somas locais
 		soma += soma_local;
 
 		for (int i = 1; i < np; i++) {
@@ -51,11 +51,11 @@ int main(int *argc, int argv[]) {
 		}
 	}
 
-	else {
+	else { // outros processos enviam suas somas locais
 		MPI_Send(&soma_local, 1, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
 	}
 
-	if (my_rank == 0) printf("Produto escalar: %f", soma);
+	if (my_rank == 0) printf("Produto escalar: %f\n", soma);
 
 	MPI_Finalize();
 }
